516db6dce8afbf7fec532f59d542bde18ea47a75
max <maksouth25@gmail.com>
2021-04-03 21:21:34 +0200

    initial commit

---
+++ src/test/java/com/harbovskyi/vast/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harbovskyi.vast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
