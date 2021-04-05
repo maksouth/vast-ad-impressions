03776d32a343a301732f2e5e7d9b573652b34537
max <maksouth25@gmail.com>
2021-04-05 19:30:20 +0200

    Created Dockerfile and docker-compose.yml
    Added app properties for setting Redis port and host,
    Removed redundant bean definition for RedisConnectionFactory

--- src/main/java/com/harbovskyi/vast/configuration/RedisTemplateConfiguration.java
+++ src/main/java/com/harbovskyi/vast/configuration/RedisTemplateConfiguration.java
@@ -3,18 +3,11 @@ package com.harbovskyi.vast.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class RedisTemplateConfiguration {
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        return new LettuceConnectionFactory(redisStandaloneConfiguration);
-    }
 
     @Bean
     public RedisTemplate<String, Long> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
