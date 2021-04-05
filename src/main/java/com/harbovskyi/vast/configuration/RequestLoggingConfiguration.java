848721eeb4ddb2aa229f7a4e1712851607f7ece5
max <maksouth25@gmail.com>
2021-04-05 19:38:56 +0200

    Added default requests logging

---
+++ src/main/java/com/harbovskyi/vast/configuration/RequestLoggingConfiguration.java
@@ -0,0 +1,19 @@
+package com.harbovskyi.vast.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfiguration {
+
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+            = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
+}
